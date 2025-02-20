d8fccd7a183daadf75c66fb20306dcb6fb463ebc
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-02-20 15:30:36 -0500

    Update

---
+++ make-window.c
@@ -0,0 +1,13 @@
+#include <gtk/gtk.h>
+
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "a-window-I-guess");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1600, 900);
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
