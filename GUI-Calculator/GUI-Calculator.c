fec7faf1c654d07952e2e9d48b1bfcb4842abe57
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-03-04 14:01:20 -0500

    Update GUI-Calculator.c

--- GUI-Calculator/GUI-Calculator.c
+++ GUI-Calculator/GUI-Calculator.c
@@ -1,1 +1,16 @@
-// will be in gtk
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <math.h>
+
+
+#define MAX_EXPR_LEN 256
+
+typedef struct {
+    GtkWidget *result_label;
+    GtkWidget *entry;
+} CalculatorWidgets;
+
+//
