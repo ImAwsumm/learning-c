b3cd1721b965d97d5c2fe706379ad2dc8e051fcc
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-03-06 00:58:38 -0500

    Update GUI-Calculator.c

--- GUI-Calculator/GUI-Calculator.c
+++ GUI-Calculator/GUI-Calculator.c
@@ -9,3 +9,14 @@
 #define MAX_EXPR_LEN 256
 
 typedef struct {
+    GtkWidget *result_label;
+    GtkWidget *entry;
+} CalculatorWidgets;
+static gboolean validate_input(const char *equation, GError **error) {
+    if (!equation || strlen(equation) == 0) {
+        
+        // Ah yes, the infamous "calculate nothing" request. Classic...
+        g_set_error(error, g_quark_from_string("CALC_ERROR"), 1,
+                    "Empty expression (I can't calculate your thoughts... yet)");
+        return FALSE;
+    }
