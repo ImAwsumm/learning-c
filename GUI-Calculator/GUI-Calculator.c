4238b022da16a0a5471dd8ead777c74b4e7e9af2
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-03-08 01:09:39 -0500

    Update GUI-Calculator.c

--- GUI-Calculator/GUI-Calculator.c
+++ GUI-Calculator/GUI-Calculator.c
@@ -4,7 +4,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <math.h>
-
 #define MAX_EXPR_LEN 256
 typedef struct {
     GtkWidget *result_label;
@@ -12,12 +11,12 @@ typedef struct {
 } CalculatorWidgets;
 static gboolean validate_input(const char *equation, GError **error) {
     if (!equation || strlen(equation) == 0) {
-        
         // Ah yes, the infamous "calculate nothing" request. Classic...
         g_set_error(error, g_quark_from_string("CALC_ERROR"), 1,
                     "Empty expression (I can't calculate your thoughts... yet)");
         return FALSE;
     }
+
     int num_count = 0;
     int op_count = 0;
     char prev_char = ' ';
@@ -37,3 +36,11 @@ static gboolean validate_input(const char *equation, GError **error) {
                            "Invalid operator sequence (They're not playing nice together)"); // when an operation can't be completed
                 return FALSE;
             }
+        } else if (*equation != ' ') {
+            g_set_error(error, g_quark_from_string("CALC_ERROR"), 1,
+                       "Invalid character (What are you doing?)");
+            return FALSE;
+        } else {
+            // A space character indicates we're no longer in a number
+            in_number = FALSE;
+        }
