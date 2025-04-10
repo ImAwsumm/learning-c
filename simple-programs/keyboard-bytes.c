2c869009b8a4d077e4e7979952dfd4c34204d1ca
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-04-09 22:18:42 -0400

    Create keyboard-bytes.c

---
+++ simple-programs/keyboard-bytes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  int a;
+  long double a;
+  printf("Size of long double = %zu bytes\n", sizeof(a));
+  printf("Size of int = %zu bytes \n", sizeof(a));
+  return 0;
+}
