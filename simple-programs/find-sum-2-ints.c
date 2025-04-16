3018b50e41a00dcc4f9ea94a9608471f5e71e3c8
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-04-15 21:06:25 -0400

    Update find-sum-2-ints.c

--- simple-programs/find-sum-2-ints.c
+++ simple-programs/find-sum-2-ints.c
@@ -5,10 +5,7 @@ int main()
 
     printf("Enter the value of n(positive integer): ");
     scanf("%d",&n);
-
-    // When you use while loop, you have to initialize the
-    // loop counter variable before the loop and increment
-    // or decrement it inside the loop
+    
     count=1;
     while(count <= n){
     	sum = sum + count;
