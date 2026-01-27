5ec463244ce559eeb5793e37b2891894f66745d9
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-01-26 22:00:51 -0500

    use system entropy in order to generate "truly random" numbers
    https://man.archlinux.org/man/getentropy.3.en

--- password/password.c
+++ password/password.c
@@ -1,7 +1,14 @@
 #include <math.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int entropy()
+{
+    unsigned int random_buffer[32];
+    getentropy(random_buffer, sizeof(random_buffer)) == 0;
+    return *random_buffer;
+}
 
 int main()
 {
@@ -22,13 +29,19 @@ int main()
     	printf("\nNot very \"random\" value is %d\n", first );
     	return 0;
     }
-    if (choice == 2 )
+    else if (choice == 2 )
     {
         srand(time(NULL)); 
 	int second = (rand() % (ub - lb + 1)) + lb;
 	//int second = srand(rand(NULL));
     	printf("\nA \"more\" random value: %d\n", second );
     	return 0;
+    }
+    else if (choice == 3 )
+    {
+	// use system entropy in order to generate a random number
+	int third = (entropy() % (ub - lb + 1)) + lb;
+	printf("\nA pretty random number: %d\n", third);
     }
     else
     {
