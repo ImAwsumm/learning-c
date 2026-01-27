702284816adb692988f1eebedc0536a0c94b27f6
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-01-26 21:33:48 -0500

    Update password.c

--- password/password.c
+++ password/password.c
@@ -5,16 +5,29 @@
 
 int main()
 {
-    printf("\n [1] Algorithm using the rand(); function \n");
-    printf("\n [2] Algorithm using the srand(); function\n");
+    // Lower bound
+    int lb = 10; 
+    // Upper bound
+    int ub = 99;
+
+    printf("\n[1] Algorithm using the rand(); function \n");
+    printf("[2] Algorithm using the srand(); function\n");
 
     int choice;
     scanf("%d", &choice);
 
     if (choice == 1 )
     {
-	int first = rand();
+	int first = (rand() % (ub - lb + 1)) + lb;
     	printf("\nNot very \"random\" value is %d\n", first );
+    	return 0;
+    }
+    if (choice == 2 )
+    {
+        srand(time(NULL)); 
+	int second = (rand() % (ub - lb + 1)) + lb;
+	//int second = srand(rand(NULL));
+    	printf("\nA \"more\" random value: %d\n", second );
     	return 0;
     }
     else
