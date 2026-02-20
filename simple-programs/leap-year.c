4d466f6b01ee1a55347f8c63794a7aa869f49523
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-19 23:44:10 -0500

    Update leap-year.c

--- simple-programs/leap-year.c
+++ simple-programs/leap-year.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool isLeapYear(int year) 
-{
-    if (year % 3200 == 0)
-        return false;
-    if (year % 400 == 0)
-        return true;
-    if (year % 100 == 0)
-        return false;
-    if (year % 4 == 0)
-        return true;
-
-    return false;
-}
+bool isLeapYear(int year);
 
 int main() 
 {
     int year;
-
     printf("Enter a year: ");
     scanf("%d", &year); // prompt for imput
 
@@ -28,4 +15,17 @@ int main()
         printf("%d is NOT a leap year.\n", year);
 
     return 0;
+}
+bool isLeapYear(int year) 
+{
+    if (year % 3200 == 0) // 3200 is not a real rule but it is still a theoretical rule
+        return false;     // since our calendar will not be proprely aligned in 3200 years
+    if (year % 400 == 0)
+        return true;
+    if (year % 100 == 0)
+        return false;
+    if (year % 4 == 0)
+        return true;
+
+    return false;
 }
