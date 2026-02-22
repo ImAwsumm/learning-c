dc98b8f93634eea21c05fdcf681000dc2533d8e5
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-21 23:16:02 -0500

    add error checking

--- simple-programs/pwd.c
+++ simple-programs/pwd.c
@@ -7,6 +7,12 @@ int main()
     char output[256];
     
     fp = popen("pwd", "r");
+    
+    if (fp == NULL) 
+    {
+        perror("popen failed");
+        return 1;
+    }
     if (fgets(output, sizeof(output), fp) != NULL) 
     {
         printf("Current directory: %s", output);
