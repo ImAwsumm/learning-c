3ce1857822bd7657955e2f071516d10ce3d603eb
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-08 01:51:12 -0400

    Update russian-roulette.c

--- stupid-code/russian-roulette.c
+++ stupid-code/russian-roulette.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(void)
+{
+    printf("Press 1 to kill a process\n");
+
+    int input;
+    scanf("%d", input);
+
+    if (input == 1)
+    {
+	//snprintf(cmd, sizeof(cmd),
+	//	"sudo kill -9 $(ps -e -o pid= | shuf -n 1)"
+    }
+
+    return 0;
+}
