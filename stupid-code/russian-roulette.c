a921eee2f788f153203c94f65c31304fce3d0446
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-12 20:03:45 -0400

    feat: kill processes
    this will now be a dangerous script killing a random process on a user's system.
    
    it is not a good idea to execute this on your system ­— instead, you should try it in a vm!

--- stupid-code/russian-roulette.c
+++ stupid-code/russian-roulette.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     printf("Press 1 to kill a process\n");
 
     int input;
-    scanf("%d", input);
+    scanf("%d ", &input);
 
     if (input == 1)
     {
-	//snprintf(cmd, sizeof(cmd),
-	//	"sudo kill -9 $(ps -e -o pid= | shuf -n 1)"
+	// allocate the exact amount of memory to the command
+	int mem_needed = snprintf(NULL, 0, "sudo kill -9 $(ps -e -o pid= | shuf -n 1)") + 1;
+
+	char *cmd = malloc(mem_needed);
+
+	snprintf(cmd, mem_needed,
+		"sudo kill -9 $(ps -e -o pid= | shuf -n 1)");
+	system(cmd);
+
+	// free the memory previously allocated to the command
+	free(cmd);
     }
 
     return 0;
