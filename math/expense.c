be9981545af6dd4816fddccd71f6cd160568cf5b
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-14 18:04:23 -0400

    feat: init

---
+++ math/expense.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//#include <string.h>
+
+int main(void)
+{
+    char *u_input;
+
+    printf("Type your expense: ");
+    scanf("%s ", &u_input);
+    printf("\n");
+
+    // logic goes here
+
+    return 0;
+}
