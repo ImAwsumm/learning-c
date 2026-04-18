43e64fb3e97fdcb2f21329742a127c2402d2e1c5
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-17 21:36:23 -0400

    feat: add command line argument parsing
    use strlol to convert strings to long integers

--- math/dividing.c
+++ math/dividing.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define EXP_ARGS 3
 
 int main(int argc, char *argv[])
 {
-    int u_input;
-    int u_divider;
+    long int u_input;
+    long int u_divider;
 
-    if (argc < 3)
+    if (!(argc == EXP_ARGS))
     {
 	printf("Type value\n");
-    	scanf("%d", &u_input);
+    	scanf("%ld", &u_input);
 
 	printf("Type divider\n");
-    	scanf("%d", &u_divider);
+    	scanf("%ld", &u_divider);
+    }
+    else
+    {
+	char *endptr;
+	char *str_1 = argv[1];
+	u_input = strtol(str_1, &endptr, 10);
+
+	char *endptr_t;
+	char *str_2 = argv[2];
+	u_divider = strtol(str_2, &endptr_t, 10);
     }
 
-    printf("You entered %d\n", u_input);
+    printf("You entered %ld\n", u_input);
 
-    printf("Divide by %d\n", u_divider);
+    printf("Divide by %ld\n", u_divider);
     return 0;
 }
