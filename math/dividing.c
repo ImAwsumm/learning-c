a58f841903daeca6efaed7d2506bb087a2de7194
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-17 20:28:06 -0400

    Update dividing.c

--- math/dividing.c
+++ math/dividing.c
@@ -4,10 +4,13 @@
 int main()
 {
     int u_input;
-
     scanf("%d", &u_input);
 
-    printf("%d\n", u_input);
+    printf("You entered %d\n", u_input);
+
+    int u_divider;
+    scanf("%d", &u_divider);
+    printf("Divide by %d\n", u_divider);
 
     return 0;
 }
