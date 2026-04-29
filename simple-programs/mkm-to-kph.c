97da04ef90bf7a6c3f715a8819d88c2dcf4c28ed
imawsumm <imawsumm@users.noreply.github.com>
2026-04-28 22:23:37 -0400

    feat: reimplement in parse_values()
    makes the program more scalable

--- simple-programs/mkm-to-kph.c
+++ simple-programs/mkm-to-kph.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define min_args 2
 #define period_length 60
+
+bool ignore_errors = false;
+
+void parse_values(int arg_offset, int number_of_values, char *arguments[]);
 
 int main(int argc, char *argv[])
 {
@@ -10,44 +15,52 @@ int main(int argc, char *argv[])
     {
 		int args_before_values = 1;
     	int num_values = argc - args_before_values;
-    	long double all_values[num_values];
 
-		int index = 0;
-		/* convert string to long double with strlol */
-		for (int i = args_before_values; i < argc; i++)
-		{
-			long double u_value = strtold(argv[i], NULL);
-			/* write all values to the all_values array */
-			all_values[index] = u_value;
-			index++;
-		}
-
-		/* calculate the results */
-    	long double valid_results[num_values];
-    	int num_valid_results = 0;
-
-    	for (int i = 0; i < num_values; i++)
-    	{
-    	    long double input = all_values[i];
-    	    if (!(input > 0 || input < 0))
-    	    {
-    	        printf("Invalid value %Lf\n", input);
-    	        return -1;
-    	    }
-
-    	    long double result = period_length / input;
-    	    valid_results[num_valid_results] = result;
-
-    	    num_valid_results++;
-    	}
-
-    	/* print values from the array */
-    	for (int i = 0; i < num_valid_results; i++)
-    	{
-    	    long double value = valid_results[i];
-    	    printf("%.19Lf\n", value);
-    	}
+		parse_values(args_before_values, num_values, argv);
     }
 
     return 0;
+}
+
+void parse_values(int arg_offset, int number_of_values, char *arguments[])
+{
+    long double all_values[number_of_values];
+	/* convert string to long double with strlol */
+
+	//int n_all_values_index = number_of_values - 1;
+	for (int i = 0; i < number_of_values; i++)
+	{
+		long double u_value = strtold(arguments[arg_offset + i], NULL);
+		/* write all values to the all_values array */
+		all_values[i] = u_value;
+	}
+	
+	/* calculate the results */
+	long double valid_results[number_of_values];
+	int num_valid_results = 0;
+	
+	for (int i = 0; i < number_of_values; i++)
+	{
+	    long double input = all_values[i];
+	    if (!(input > 0 || input < 0))
+	    {
+	        printf("Invalid value detected at index %d: %Lf \n", i, input);
+			if(!ignore_errors)
+			{
+				exit(-1);
+			}
+	    }
+	
+	    long double result = period_length / input;
+	    valid_results[num_valid_results] = result;
+	
+	    num_valid_results++;
+	}
+	
+	/* print values from the array */
+	for (int i = 0; i < num_valid_results; i++)
+	{
+	    long double value = valid_results[i];
+	    printf("%.19Lf\n", value);
+	}
 }
