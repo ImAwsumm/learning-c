584a1b024e221caa84b5a70a6918cd6e052668b1
imawsumm <imawsumm@users.noreply.github.com>
2026-04-28 20:44:33 -0400

    fix: add function prototype
    Function prototypes were missing at the top of the switch-fallthrough.c
    this simply adds a function prototype in order to prevent implicit
    function declaration
    ISO C99 and later do not support the implicit declarations

--- simple-programs/switch-fallthrough.c
+++ simple-programs/switch-fallthrough.c
@@ -2,8 +2,11 @@
 
 float pver;
 
+void func_one(void);
+void func_two(void);
+void func_the(void);
 
-int main()
+int main(void)
 {
     printf("\n Type the current version:");
     scanf(" %f", &pver);
@@ -11,41 +14,40 @@ int main()
     int y = (int)(pver * 100);
     switch (y) 
     {
-    case 141:
-        printf("Case 1 is Matched.\n");
-	func-one();
-	__attribute__ ((fallthrough));
+	case 141:
+    	    printf("Case 1 is Matched.\n");
+    	    func_one();
+    	    __attribute__ ((fallthrough));
 
-    case 200:
-        printf("Case 2 is Matched.\n");
-	func-two();
-	__attribute__ ((fallthrough));
+    	case 200:
+    	    printf("Case 2 is Matched.\n");
+    	    func_two();
+    	    __attribute__ ((fallthrough));
 
-    case 300:
-        printf("Case 3 is Matched.\n");
-	func-the();
-	__attribute__ ((fallthrough));
+    	case 300:
+    	    printf("Case 3 is Matched.\n");
+    	    func_the();
 
-	goto end;
-    default:
-        printf("Default case is Matched.\n");
+    	    goto end;
+    	default:
+    	    printf("Default case is Matched.\n");
 
-    end:
-	printf("Update completed!\n");
+    	end:
+	    printf("Update completed!\n");
 
     }
     return 0;
 }
 
-void func-one()
+void func_one(void)
 {
     printf("Hey\n");
 }
-void func-two()
+void func_two(void)
 {
     printf("How\n");
 }
-void func-the()
+void func_the(void)
 {
     printf("Are you?\n");
 }
