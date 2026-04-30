6903a6e265888ccfd1d534d251f59489b62e79e0
imawsumm <imawsumm@users.noreply.github.com>
2026-04-29 21:57:44 -0400

    refactor: remove useless variables

--- memory-management/f-malloc.c
+++ memory-management/f-malloc.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
 	int y;
 	int *x = malloc(4);
 	x = &y;
 	y = 123;
+
+	printf("%d\n", *x); /* dereference the x pointer to y */
+
     return 0;
 }
