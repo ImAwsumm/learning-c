4d520f1bbef6545da06ed19bbc12b69a1e7e1d38
imawsumm <imawsumm@users.noreply.github.com>
2026-05-03 20:24:30 -0400

    feat: add content to f-malloc.c

--- memory-management/f-malloc.c
+++ memory-management/f-malloc.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+#define test_number 123
+
+int main(int argc, char *argv[])
 {
-	int y;
-	int *x = malloc(4);
+	if (argc > 1)
+	{
+		auto char *str = argv[1];
+		auto char *endptr;
+		auto long int number;
+
+		number = strtol(str, &endptr, 10);
+		if (!(*endptr != '\0'))
+		{
+			printf("number is: %ld\n", number);
+		}
+		auto int array_elements = argc - 1;
+		auto int numbers[array_elements];
+	}
+
+	auto int y;
+	auto int *x = malloc(4);
+
 	x = &y;
-	y = 123;
+	y = test_number;
 
 	printf("%d\n", *x); /* dereference the x pointer to y */
 
