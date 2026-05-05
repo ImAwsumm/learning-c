e3b7a1e8e03e4bdf4fc840e07c9b2538633fa042
imawsumm <imawsumm@users.noreply.github.com>
2026-05-05 18:06:44 -0400

    feat: add error checking (square.c)
    add more error checking for the user input in order to validate
    everything before executing anything with it

--- command-parsing/square.c
+++ command-parsing/square.c
@@ -4,43 +4,63 @@
 
 #define max_word_size 32
 
+void invalid_cmd_message(void);
+
 int main(int argc, char *argv[])
 {
 	int base_args_n = 1;
 	bool valid_args = false;
 
 	long x,y;
-	if (argc > 1)
+
+	while (1)
 	{
-		if (argc > 2)
+		if (argc > 1)
 		{
-			if (argc == 3)
+			if (argc > 2)
 			{
-				char *endptr;
-				int index = base_args_n;
-				x = strtol(argv[index], &endptr, 10);
-				index++;
-				y = strtol(argv[index], &endptr, 10);
-				valid_args = true;
+				if (argc == 3)
+				{
+					char *endptr;
+					int index = base_args_n;
+					x = strtol(argv[index], &endptr, 10);
+					if (!(*endptr == '\0'))
+					{
+						invalid_cmd_message();
+						break;
+					}
+					index++;
+					y = strtol(argv[index], &endptr, 10);
+					if (!(*endptr == '\0'))
+					{
+						invalid_cmd_message();
+						break;
+					}
+					else
+					{
+						/* all variables taken from the user are valid */
+						valid_args = true;
+						break;
+					}
+				}
+				else
+				{
+					invalid_cmd_message();
+					break;
+				}
 			}
 			else
 			{
-				printf("You did not type the correct amount of arguments\n");
-				printf("The command should be structured as follows:\n");
-				printf("./[binary_name] [x axis] [y axis]\n\n");
+				/* continue despite the lack of arguments */
+				invalid_cmd_message();
+				break;
 			}
-		}
-		else
-		{
-			/* continue despite the lack of arguments */
-			printf("Missing arguments for the full command\n");
-
 		}
 	}
 
 	char *word_to_print = "Hi";
 
-	int buf_size_word = 1 + snprintf(NULL, 0, word_to_print);
+	int buf_size_word = 1 + snprintf(NULL, 0, "%s", word_to_print);
 
 	if (buf_size_word >= max_word_size)
 	{
@@ -48,10 +68,15 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	/* initialize the word buffer
+	 * then write the word to it while making sure we aren't overflowing the buffer */
     char word[max_word_size];
 
-    snprintf(word, (size_t)buf_size_word, word_to_print);
+    snprintf(word, (size_t)buf_size_word, "%s", word_to_print);
 
+	/* get the user input for the x and y axis
+	 * we are doing "if (!valid_args)" because valid args is only true if the input is valid 
+	 * the input could have already been taken with the command line arguments */
 	if (!valid_args)
 	{
 		printf("Type the X axis: ");
@@ -63,6 +88,7 @@ int main(int argc, char *argv[])
     	scanf("%ld", &y);
 	}
 
+	/* errors out if the user is trying to print nothing */
     if (!(x > 0) || !(y > 0))
     {
 		printf("No characters to print out\n");
@@ -79,5 +105,12 @@ int main(int argc, char *argv[])
 		}
 		printf("\n"); /* print newline because we did a full line */
     }
-    return 0;
+	return 0;
+}
+
+void invalid_cmd_message(void)
+{
+	printf("You did not type correct arguments.\n");
+	printf("The command should be structured as follows:\n");
+	printf("./[binary_name] [x axis] [y axis]\n\n");
 }
