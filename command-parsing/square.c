79aac878d5a2c49ff645d23b8e567cb1e878e0be
imawsumm <imawsumm@users.noreply.github.com>
2026-05-05 17:38:11 -0400

    feat: add command line arg parsing
    parses through the cli arguments in order to then treat them as longs

--- command-parsing/square.c
+++ command-parsing/square.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define max_word_size 32
 
-/*
 int main(int argc, char *argv[])
-*/
-int main(void)
 {
+	int base_args_n = 1;
+	bool valid_args = false;
+
+	long x,y;
+	if (argc > 1)
+	{
+		if (argc > 2)
+		{
+			if (argc == 3)
+			{
+				char *endptr;
+				int index = base_args_n;
+				x = strtol(argv[index], &endptr, 10);
+				index++;
+				y = strtol(argv[index], &endptr, 10);
+				valid_args = true;
+			}
+			else
+			{
+				printf("You did not type the correct amount of arguments\n");
+				printf("The command should be structured as follows:\n");
+				printf("./[binary_name] [x axis] [y axis]\n\n");
+			}
+		}
+		else
+		{
+			/* continue despite the lack of arguments */
+			printf("Missing arguments for the full command\n");
+
+		}
+	}
+
 	char *word_to_print = "Hi";
 
 	int buf_size_word = 1 + snprintf(NULL, 0, word_to_print);
@@ -21,16 +51,17 @@ int main(void)
     char word[max_word_size];
 
     snprintf(word, (size_t)buf_size_word, word_to_print);
-    printf("Type the X axis: ");
 
-    int x;
-    scanf("%d", &x);
-    printf("Type the y axis: ");
+	if (!valid_args)
+	{
+		printf("Type the X axis: ");
+    	scanf("%ld", &x);
 
-    while (getchar() != '\n'); /* clear the stdin input buffer */
-    int y;
-    scanf("%d", &y);
-    printf("\n");
+    	while (getchar() != '\n'); /* clear the stdin input buffer */
+
+    	printf("Type the y axis: ");
+    	scanf("%ld", &y);
+	}
 
     if (!(x > 0) || !(y > 0))
     {
@@ -38,13 +69,15 @@ int main(void)
 		exit(1);
     }
 
+    printf("\n"); /* print newline for clarity */
+
     for (int i = 0; i < y; i++)
     {
 		for (int j = 0; j < x; j++)
 		{
 		    printf("%s ", word);
 		}
-		printf("\n");
+		printf("\n"); /* print newline because we did a full line */
     }
     return 0;
 }
