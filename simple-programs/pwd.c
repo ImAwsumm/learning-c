cb056bbef1aa0e01a6e51a6c4d290999989f3564
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-05-06 10:58:45 -0400

    refactor: remove duplicate print statements
    Replace them with a single function
    print directory()

--- simple-programs/pwd.c
+++ simple-programs/pwd.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void print_directory(char current_working_dir[256]);
+
+int main(void)
 {
     FILE *fp;
     char output[256];
-    
+
+    /* execute pwd and store the output in fp file */
     fp = popen("pwd", "r");
     
     if (fp == NULL) 
@@ -13,12 +16,19 @@ int main()
         perror("popen failed");
         return 1;
     }
+    /* read file fp using fgets and store all data in the output string */
     if (fgets(output, sizeof(output), fp) != NULL) 
     {
-        printf("Current directory: %s", output);
+        print_directory(output);
     }
-    pclose(fp);
+    pclose(fp); /* close unused file */
+
+    print_directory(output);
     
-    printf("Current directory: %s", output);
     return 0;
+}
+
+void print_directory(char current_working_dir[256])
+{
+    printf("Current directory: %s\n", current_working_dir);
 }
